6b21d4b0e876d81ef6bab5c75a8ddf74e4ce916c
walidDev <47478357+walidza@users.noreply.github.com>
2020-04-02 04:59:20 +0100

    Add files via upload

---
+++ mysql/s_mysql.lua
@@ -0,0 +1,29 @@
+addEventHandler("onResourceStart",root,
+function ()
+    DBConnection = dbConnect( "mysql", "dbname=rp;host=127.0.0.1;charset=utf8", "root", "iwalidzazarrouki" )
+end
+)
+
+
+function singleQuery(str,...)
+	if (DBConnection) then
+		local query = dbQuery(DBConnection,str,...)
+		local result = dbPoll(query,-1)
+		if (type(result == "table")) then
+			return result[1]
+		else
+			return result
+		end
+	else
+		return false
+	end
+end
+
+function execute(str,...)
+	if (DBConnection) then
+		local query = dbExec(DBConnection,str,...)
+		return query
+	else
+		return false
+	end
+end
